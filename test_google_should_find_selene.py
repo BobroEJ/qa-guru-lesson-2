cfc213a5e9e7d5508edc04ed89c5f7ad88215f8f
bobroej <tverdune@yandex.ru>
2022-06-20 23:07:59 +0300

    Lesson 2 var 1 (w/o autouse)

---
+++ test_google_should_find_selene.py
@@ -0,0 +1,13 @@
+import pytest
+from selene.support.shared import browser
+from selene import be, have
+
+@pytest.fixture()
+def browser_size():
+    browser.config.window_width = 800
+    browser.config.window_height = 600
+
+def test_lesson2(browser_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('selene: User-oriented Web UI browser tests in Python'))
