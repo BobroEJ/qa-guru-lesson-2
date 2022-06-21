55d4c1fc5c34a99a5faf3307014aa04a4c3a771e
bobroej <tverdune@yandex.ru>
2022-06-21 09:38:34 +0300

    Lesson 2 final

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,15 +2,17 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-@pytest.fixture()
-def browser_size(scope='session', autouse=True):
+@pytest.fixture(scope='session', autouse=True)
+def browser_size():
     browser.config.window_width = 800
     browser.config.window_height = 600
 
-def test_selene_in_google(browser_size):
+
+def test_selene_in_google():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('selene: User-oriented Web UI browser tests in Python'))
+
 
 def test_selene_in_google_fail():
     browser.open('https://google.com')
